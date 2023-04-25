124b0e6afc488471ccc38881fe17a0190ac43c45
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-04-26 01:27:32 +0300

    hw

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorButton = widget.querySelector('.change-color');
+const body = document.body;
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
